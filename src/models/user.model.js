cb5fc92d9ba638c6c53c66bcf3f392bc4861f6ca
vansh-cryp <vanshgusain920@gmail.com>
2025-07-05 20:57:34 +0530

    data model finalized

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,7 +47,7 @@ const userSchema = new Schema({
     }
 
 },{timestamps:true});
-
+//jwt is a berarer token like a master key it gives data to any person who has this token
 //run this code only if password is changed
 userSchema.pre("save",async function(next){
     if(this.isModified("password")){
@@ -59,6 +59,24 @@ userSchema.pre("save",async function(next){
 });
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAccessToken= function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+userSchema.methods.generateRefreshToken= function(){
+
+    return jwt.sign({
+        _id:this._id,
+    },process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
 }
 
 export const User = mongoose.model("User",userSchema);
